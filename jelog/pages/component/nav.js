d993373996b356c6f8d6cc0a98f0fa5288a93d45
ParkJH-Front <100084383+ParkJH-Front@users.noreply.github.com>
2022-12-02 17:40:44 +0900

    시작

---
+++ jelog/pages/component/nav.js
@@ -0,0 +1,15 @@
+export default function nav() {
+  return (
+    <nav>
+      <section className="nav_layout">
+        <a>
+          <p>logo</p>
+        </a>
+        <p>Jelog</p>
+        <a>
+          <p>light</p>
+        </a>
+      </section>
+    </nav>
+  );
+}
---
+++ jelog/pages/index.js
@@ -0,0 +1,10 @@
+import Nav from "./component/nav.js";
+
+export default function Home() {
+  return (
+    <div>
+      <Nav />
+      <p>hi</p>
+    </div>
+  );
+}
