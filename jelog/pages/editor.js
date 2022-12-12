016b1e7e848a64c736896697153b46b27a18b237
ParkJH-Front <100084383+ParkJH-Front@users.noreply.github.com>
2022-12-12 17:27:41 +0900

    12.12
    editor 구상중

--- jelog/pages/editor.js
+++ jelog/pages/editor.js
@@ -1,26 +1,32 @@
+import React, { useRef, useState } from "react";
+
 export default function editor() {
-  // const content = [
-  // {
-  //   line: 0,
-  //   content: "안녕하세요",
-  //   tag: "h1",
-  //   style: "bold",
-  //   classname: "editor_head",
-  //   flag: 1,
-  // },
-  // {
-  //   line: 1,
-  //   content: "반갑습니다.",
-  //   tag: "p",
-  //   style: "",
-  //   classname: "editor_main",
-  //   flag: 1,
-  // },
-  // ]
+  const [text, setText] = useState("");
+  const [lineText, setLineText] = useState([]);
+  const [lineTag, setLineTag] = useState([]);
+  const [lineClass, setLineClass] = useState([]);
+
+  const onInput = (e) => {
+    // console.dir(e.target.children); // 자식 요소들을 가지고 올 수 있음.
+  //   setLineText(e.target.children[1]?.innerText);
+  //   setLineTag(e.target.children[1]);
+  //   setLineClass(e.target.children[1]?.className);
+
+  //   setText(e.target.innerText);
+  // };
+  // console.log(`내용 : ${lineText} / 태그 : ${lineTag} / 클래스 : ${lineClass}`);
+  // console.log(text);
 
   return (
     <div>
-      <div contenteditable="true"></div>
+      <div
+        contentEditable="true" // html 태그를 텍스트 입력 가능하도록 하는 옵션
+        suppressContentEditableWarning // 하위 요소들을 수정 할 수 있다는 경고문을 제거 해줌. (그 외 모든 경고문 제외 가능)
+        onInput={onInput}
+      ></div>
+      <div contentEditable="false" suppressContentEditableWarning>
+        {text}
+      </div>
     </div>
   );
 }
