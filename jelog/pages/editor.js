9d3fd72e4fb80f3d94706aff1d413d2983037edd
ParkJH-Front <100084383+ParkJH-Front@users.noreply.github.com>
2022-12-13 17:45:42 +0900

    12.13
    editor 입력값을 object로 받도록 설정 완료.
    출력부에 html tag 및 속성들 적용하여 출력중인 방법 고안.

--- jelog/pages/editor.js
+++ jelog/pages/editor.js
@@ -1,11 +1,40 @@
-import React, { useRef, useState } from "react";
+import React, { createElement, useEffect, useRef, useState } from "react";
 import stlye from "./style/editor.module.css";
 export default function editor() {
-  const [text, setText] = useState([]);
+  const [text, setText] = useState(""); // contentEditable 박스 내 상태관리 state
+  const testObject = []; // 최종 object 화 할 array
 
+  // contentEditable 박스 내 입력값이 있는 경우 test state 로 전달.
   const onInput = (e) => {
     setText(e.target.innerText);
   };
+
+  // test state 의 입력값을 \n(엔터, 줄바꿈) 기준으로 나눔
+  const contentBlock = text.split("\n");
+
+  // 줄단위로 나눠진 입력값을 object 화
+  contentBlock.map((contant, index) => {
+    testObject.push({
+      line: index, //줄번호
+      contant: contant, //내용
+      tag: "p", //적용할 HTML TAG, 기본은 p
+      style: "", //적용할 CSS 스타일, 기본은 ""
+    });
+  });
+
+  // 아이디어 : testObject 가 변경될 때 마다, 출력박스에 해당 element를 생성한다.
+  // 1. useEffect() 활용 / 실패 실제로 element가 생성이 안됨.
+  // {
+  //   useEffect(() => {
+  //     testObject.map((item) => {
+  //       ReactDom.rander(
+  //         React.createElement(`${item.tag}`, {
+  //           children: `${item.contant}`,
+  //         })
+  //       );
+  //     });
+  //   });
+  // }
 
   return (
     <div>
@@ -14,9 +43,18 @@ export default function editor() {
         <button>H2</button>
         <button>H3</button>
         <button>H4</button>
-        <button>굵게</button>
-        <button>기울기</button>
-        <button>가로선</button>
+        <button>
+          <b>굵게</b>
+        </button>
+        <button>
+          <i>기울기</i>
+        </button>
+        <button>
+          <u>밑줄</u>
+        </button>
+        <button>
+          <s>가로선</s>
+        </button>
         <button>코드</button>
         <button>인용구</button>
         <button>이미지</button>
@@ -30,9 +68,7 @@ export default function editor() {
             onInput={onInput}
           ></div>
         </div>
-        <div contentEditable="false" suppressContentEditableWarning>
-          {text}
-        </div>
+        <div contentEditable="false" suppressContentEditableWarning></div>
       </div>
       <div>
         <button>저장</button>
