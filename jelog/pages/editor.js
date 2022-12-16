fbd6ac76765cc7a640901949d8af1fa00024450c
ParkJH-Front <100084383+ParkJH-Front@users.noreply.github.com>
2022-12-16 17:51:23 +0900

    12.16
    삼항연산자의 발견!
    Quill 에디터를 사용해봣으나 맘에들지않음,
    수동으로다시 만들며 XSS 를 대비하자.

--- jelog/pages/editor.js
+++ jelog/pages/editor.js
@@ -1,5 +1,22 @@
-import React, { createElement, useEffect, useRef, useState } from "react";
-import stlye from "./style/editor.module.css";
-export default function editor() {
-  return <div>시발</div>;
+import { useState } from "react";
+
+export default function Editor() {
+  const [text, setText] = useState("");
+  const [contentSource, setContentSource] = useState([]);
+  const content = (event) => {
+    setText(event.target.innerHTML);
+  };
+
+  return (
+    <container>
+      <div
+        contentEditable="true"
+        suppressHydrationWarning
+        onChange={content}
+      ></div>
+      <div contentEditable="false" suppressHydrationWarning>
+        {text}
+      </div>
+    </container>
+  );
 }
--- jelog/pages/index.js
+++ jelog/pages/index.js
@@ -1,5 +1,6 @@
 import Nav from "./component/nav";
 import Main from "./component/main";
+
 export default function Home() {
   return (
     <div>
